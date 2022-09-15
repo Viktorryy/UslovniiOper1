899ee80cfc955821e7c3e7070beed184ba99ca5a
Viktorryy <viktory_o@rambler.ru>
2022-09-15 11:46:59 +0400

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Задача 2
+        int age = 30;
+        if (age >=7 && age < 18)  {
+            System.out.println("Ребенок ходит в школу"); }
+        if (age >= 18 && age < 24)  {
+            System.out.println("Человек уже закончил школу и может отправляться в университет"); }
+        if (age >= 24)  {
+            System.out.println("Человек окончил университет и ему пора искать первую работу"); }
     }
 }
