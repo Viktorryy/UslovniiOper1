fd2d791123c114730340eeb8b6d55f19ede7d2f3
Viktorryy <viktory_o@rambler.ru>
2022-09-15 12:44:51 +0400

    Задача 6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Задача 6
+        int placeSeat = 60;
+        int placeStand = 102;
+        int numberPersons = 1000;
+
+        if (numberPersons <=placeSeat)  {
+            System.out.println("В вагоне есть сидячее место");
+        }
+        else {
+            if (numberPersons > placeSeat && numberPersons <= placeStand) {
+                System.out.println("В вагоне есть только стоячее место");
+            }
+            else {System.out.println("Вагон переполнен, мест - нет!"); }
+            }
+
+
     }
 }
