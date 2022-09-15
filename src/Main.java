690d1320dc0706404939aeb09f3697f8c5ac39cf
Viktorryy <viktory_o@rambler.ru>
2022-09-15 11:33:38 +0400

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        // Задача 1
+        int age = 19;
+        if (age >=18)  {
+           System.out.println("Вы совершеннолетний!"); }
+        if (age < 18)  {
+            System.out.println("Возраст совершеннолетия ещё не наступил, " +
+                    "и нужно немного подождать!"); }
+
+
     }
 }
