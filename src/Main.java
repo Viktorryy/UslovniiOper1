730801a82697e31a9cd207b2c3662ab3d416fba7
Viktorryy <viktory_o@rambler.ru>
2022-09-15 13:37:18 +0400

    Домашнее задание - 3 Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Задача 6
-        int placeSeat = 60;
-        int placeStand = 102;
-        int numberPersons = 1000;
+        // Домашнее задание - 3
+        //Задание 1
 
-        if (numberPersons <=placeSeat)  {
-            System.out.println("В вагоне есть сидячее место");
-        }
-        else {
-            if (numberPersons > placeSeat && numberPersons <= placeStand) {
-                System.out.println("В вагоне есть только стоячее место");
+        float age =(float) 1;
+        boolean ageDetSad = age >=2 && age < 7; // Возраст для детского сада
+        boolean ageSchool = age >=7 && age < 19; // Возраст для школы
+        boolean ageUniver = age >=19 && age < 25; // Возраст для университета
+        boolean ageWork = age >=25;  // Возраст для работы
+        boolean ageMl = age <2;  // Возраст для дома
+
+
+        if (ageDetSad) {System.out.println("Если возраст человека равен "+  age + " ,то ему нужно ходить в детский сад");}
+        if (ageSchool) {System.out.println("Если возраст человека равен "+  age + " ,то ему нужно ходить в школу");}
+        if (ageUniver) {System.out.println("Если возраст человека равен "+  age + " ,то ему нужно ходить в университет");}
+        if (ageWork) {System.out.println("Если возраст человека равен "+  age + " ,то ему нужно ходить на работу!!");}
+        if (ageMl) {System.out.println("Если возраст человека равен "+  age + " ,то Сиди дома с мамой!!");}
+
             }
-            else {System.out.println("Вагон переполнен, мест - нет!"); }
-            }
-
-
-    }
 }
